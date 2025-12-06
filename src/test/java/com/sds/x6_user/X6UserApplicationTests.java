97374b177374f31c25fa969a758f1f1336a9a16c
MaxWarren9 <maximvoinoff@gmail.com>
2025-12-06 13:22:03 +0300

    init

---
+++ src/test/java/com/sds/x6_user/X6UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sds.x6_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class X6UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
